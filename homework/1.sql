7bfdc5ce7f3434f1a4944546b9180f58e92fbd63
Nikita Melnikov <nikmel2803@gmail.com>
2021-06-11 22:11:58 +0300

    add homework

--- homework/1.sql
+++ homework/1.sql
@@ -1,1 +1,2 @@
-SELECT sum(price * quantity) AS Result FROM shop.dbo.order_details WHERE order_id IN (SELECT ID FROM shop.dbo.orders WHERE completion_date >= '2022-01-30 20:02:23')
+SELECT sum(price * quantity) AS Result FROM shop.dbo.order_details
+    WHERE order_id IN (SELECT ID FROM shop.dbo.orders WHERE completion_date >= '2022-01-30 20:02:23')
